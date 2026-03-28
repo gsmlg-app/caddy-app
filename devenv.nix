316c371ee80085cfc051dc463b8eae506325e545
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-28 19:34:37 +0800

    chore(devenv): enable unfree packages and android sdk licenses
    - Configure allowUnfree and android_sdk.accept_license for nixpkgs
    - Add androidSdk acceptLicense to devenv.yaml
    - Add pkg-config and libsecret dependencies
    - Disable android-studio and add explicit extra licenses

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, config, inputs, ... }:
 
 let
-  pkgs-stable = import inputs.nixpkgs-stable { system = pkgs.stdenv.system; };
-  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+  pkgs-stable = import inputs.nixpkgs-stable { system = pkgs.stdenv.system; config = { allowUnfree = true; android_sdk.accept_license = true; }; };
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; config = { allowUnfree = true; android_sdk.accept_license = true; }; };
 in
 {
   env.GREET = "Caddy App";
@@ -19,12 +19,19 @@ in
     pkgs-stable.gomobile
     pkgs-stable.xcaddy
     pkgs-stable.gnumake
+    pkgs.pkg-config
+    pkgs.libsecret
   ];
 
   android = {
     enable = true;
     flutter.enable = true;
     flutter.package = pkgs-unstable.flutter;
+    android-studio.enable = false;
+    extraLicenses = [
+      "android-sdk-license"
+      "android-sdk-preview-license"
+    ];
   };
 
   scripts.hello.exec = ''
